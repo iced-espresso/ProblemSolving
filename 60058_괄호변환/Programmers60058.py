49dec41172b89b43485cf876937354bc578d845b
iced-espresso <csu2018@gmail.com>
2022-07-09 23:44:00 +0900

    add 60058_괄호변환/Programmers60058.py

---
+++ 60058_괄호변환/Programmers60058.py
@@ -0,0 +1,49 @@
+def checkRightStr(u):
+    leftCnt = 0
+    rightCnt = 0
+    for c in u:
+        if c == '(':
+            leftCnt += 1
+        else:
+            rightCnt += 1
+        if leftCnt < rightCnt:
+            return False
+    return leftCnt == rightCnt
+
+def flip(x):
+    flipHash = {"(":")", ")":"("}
+    return flipHash[x]
+
+def change(w):
+    if w == "": return w
+    u, v = splitBalenced(w)
+    if checkRightStr(u):
+        return u + change(v)
+    else:
+        if u == "":
+            u = "(" + change(v) + ")"
+        else:
+            u = "".join([flip(x) for x in u[1:-1]])
+            u = "(" +  change(v) + ")" + u
+        return u
+
+    
+def splitBalenced(w):
+    leftCnt = 0
+    rightCnt = 0
+    for i, c in enumerate(w):
+        if c == '(':
+            leftCnt += 1
+        else:
+            rightCnt += 1
+        if leftCnt == rightCnt:
+            return (w[0:i+1], w[i+1:])
+
+def solution(p):
+    answer = p if checkRightStr(p) else change(p)
+    return answer
+
+
+solution("(()())()")
+solution(")(")
+solution("()))((()")
