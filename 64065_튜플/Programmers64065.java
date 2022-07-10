3bffcca95e31e769af5beb578df8b7707a68329a
iced-espresso <csu2018@gmail.com>
2022-07-10 13:07:07 +0900

    refactoring 64065_튜플/Programmers64065.java

--- 64065_튜플/Programmers64065.java
+++ 64065_튜플/Programmers64065.java
@@ -1,33 +1,61 @@
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
 public class Programmers64065 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Solution solution = new Solution();
         int[] answer = solution.solution("{{1,2,3},{2,1},{1,2,4,3},{2}}");
         System.out.println(answer);
     }
 }
 
+class Tuple {
+    private ArrayList<Integer> tupleList = new ArrayList<Integer>();
+    private HashMap<Integer, Integer> tupleMap = new HashMap<Integer, Integer>();
+    ;
+
+    public Tuple() {
+
+    }
+
+    public Tuple(String tupleStr) {
+        for (String element : tupleStr.split(",")) {
+            this.addElement(Integer.parseInt(element));
+        }
+    }
+
+    public void addElement(Integer element) {
+        if (!tupleMap.containsKey(element)) {
+            tupleMap.put(element, 1);
+            tupleList.add(element);
+        }
+    }
+
+    public void addTuple(Tuple otherTuple) {
+        for (Integer otherElement : otherTuple.tupleList) {
+            addElement(otherElement);
+        }
+    }
+
+    public int[] toArray() {
+        return tupleList.stream().mapToInt(i -> i).toArray();
+    }
+
+}
+
 class Solution {
     public int[] solution(String s) {
         HashMap<String, Integer> map = new HashMap<String, Integer>();
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-        s = s.substring(2,s.length()-2);
-        String[] splited = s.split("},[{]");
-        Arrays.sort(splited, (String s1, String s2) -> s1.length() - s2.length());
-        for(String ss:splited){
-            String[] x = ss.split(",");
-            for(String y:x){
-                if(!map.containsKey(y)){
-                    map.put(y,1);
-                    answer.add(Integer.parseInt(y));
-                }
-            }
+        Tuple answer = new Tuple();
 
+        s = s.substring(2, s.length() - 2);
+        String[] elements = s.split("},[{]");
+        Arrays.sort(elements, (s1, s2) -> s1.length() - s2.length());
+
+        for (String element : elements) {
+            answer.addTuple(new Tuple(element));
         }
-        return answer.stream().mapToInt(i->i).toArray();
+        return answer.toArray();
     }
 }
